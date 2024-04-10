6600351f776c663336d79eab51ffeee416808808
Naveenraj20 <naveenrajnaveenrajd@gmail.com>
2024-04-10 14:33:16 +0530

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import style from "./todoForm.module.css";
+
+export default function TodoForm({ todos, setTodos }) {
+  const [todo, setTodo] = useState({name:"",state:false});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({name:"",state:false});
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={style.todoForm}>
+      <div style={{display:"flex"}}>
+        <input
+          type="text"
+          onChange={(e) => setTodo({name:e.target.value,state:false})}
+          value={todo.name}
+          className={style.input}
+          placeholder="Enter Todo items..."
+        />
+        <button type="submit" className={style.btn}>
+          ADD
+        </button>
+      </div>
+    </form>
+  );
+}
