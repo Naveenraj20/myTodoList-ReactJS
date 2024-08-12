b67e04f356a9b37560943b3b9af945aae9c5bf5e
Naveenraj20 <naveenrajkingraj@gmail.com>
2024-08-12 10:10:54 +0530

    delete unwanter folders

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./footer.module.css"
-
-export default function Footer(){
-    return <footer className={style.footerBlock}>
-        <div>
-            Footer
-        </div>
-    </footer>
-}
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./header.module.css"
-
-export default function Header(){
-    return <header className={style.headerBlock}>
-        <div>
-            This is color changer
-        </div>        
-    </header>
-}
